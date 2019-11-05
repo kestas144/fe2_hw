1f3369dd1d2de44baecb9032774fb5cd5b3cb4af
Kestas <55563304+kestas144@users.noreply.github.com>
2019-11-05 15:13:28 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import App from "./components/app";
+
+
+ReactDOM.render(
+
+
+    <App/>, document.getElementById('root'));
+
+serviceWorker.unregister();
+
+
